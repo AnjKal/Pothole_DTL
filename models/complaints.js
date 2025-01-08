0a9d66278fc2f827a334ce72e6f0c17c8a63b5b4
Anjali Kal <anjaliaiml@gmail.com>
2025-01-08 10:40:57 +0530

    commiting to revert

--- models/complaints.js
+++ models/complaints.js
@@ -1,14 +1,21 @@
 const mongoose = require("mongoose");
-const schema = mongoose.Schema;
 
-const sch = new schema({
-    type:String,
-    image:String,
-    details:String,
-    severity:Number,
-    location:String,
-    comments: [{ text: String, date: Date }]
+const ComplaintSchema = new mongoose.Schema({
+    type: String,
+    details: String,
+    severity: String,
+    location: String,
+    image: String,
+    comments: [
+        {
+            text: String,
+            date: Date,
+        },
+    ],
+    status: {
+        type: String,
+        default: "Complaint Received",
+    },
 });
 
-const prob = mongoose.model("prob",sch);
-module.exports = prob; 
+module.exports = mongoose.model("Complaint", ComplaintSchema);
